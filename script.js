47e24c8fb946e524db7523b1c543bedf73dfb6e8
mateussousaa <mateussousa20@outlook.com>
2022-04-20 17:35:15 -0300

    requirement 09 - finished

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const buttonClearBoard = document.getElementById('clear-board');
+
 document.addEventListener('click', function(event) {
   if (event.target.classList.contains('color')) {
     const previousSelectedColor = document.querySelector('.selected');
@@ -12,5 +14,12 @@ document.addEventListener('click', function(event) {
     const cssObject = window.getComputedStyle(selectedColor);
     const backgroundColor = cssObject.getPropertyValue('background-color');
     event.target.style.backgroundColor = backgroundColor;
+  }
+});
+
+buttonClearBoard.addEventListener('click', function() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let pixel of pixels) {
+    pixel.style.backgroundColor = 'white';
   }
 });
