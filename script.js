f0939bfe6858ff22d7b564af8d22cfd250dc8b9c
mateussousaa <mateussousa20@outlook.com>
2022-04-20 17:21:58 -0300

    requirement 08 - finished

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ document.addEventListener('click', function(event) {
     event.target.classList.add('selected');
   }
 });
+
+document.addEventListener('click', function(event) {
+  if(event.target.classList.contains('pixel')) {
+    const selectedColor = document.querySelector('.selected');
+    const cssObject = window.getComputedStyle(selectedColor);
+    const backgroundColor = cssObject.getPropertyValue('background-color');
+    event.target.style.backgroundColor = backgroundColor;
+  }
+});
